007424dccf141c717af82eee281a37bd60525cde
FesalBadday <fesal.n04@hotmail.com>
2026-05-26 18:31:23 +0400

    tweaks

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+        compressionLevel: 8,
+      },
+    }),
+  ],
 })
